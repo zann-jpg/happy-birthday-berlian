002e8efcf2c2cf9613bab2664ffb907481b9ce78
zann-jpg <ozannsigma@gmail.com>
2026-07-28 20:52:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-
 const passwordBenar="berlian";
+
+
+let musicOn=true;
+
+
+
+// hilangkan loading
+
+setTimeout(()=>{
+
+document.getElementById("loading")
+.style.display="none";
+
+
+},2500);
+
+
+
+
 
 
 function cekPassword(){
 
 
-let password =
+let pass=
 document.getElementById("password").value;
 
 
-if(password===passwordBenar){
+
+if(pass===passwordBenar){
 
 
 document.getElementById("login")
@@ -21,8 +40,19 @@ document.getElementById("main")
 
 
 
-document.getElementById("music")
-.play();
+let music=
+document.getElementById("music");
+
+
+music.volume=.5;
+
+
+music.play();
+
+
+
+typing();
+
 
 
 buatHati();
@@ -41,7 +71,143 @@ document.getElementById("error")
 }
 
 
+
 }
+
+
+
+
+
+// TULISAN MENGETIK
+
+
+function typing(){
+
+
+let text=
+"Happy Birthday BERLIAN NAYLA RAMADHANI ALMARIF ❤️";
+
+
+let i=0;
+
+
+let speed=70;
+
+
+let timer=setInterval(()=>{
+
+
+document.getElementById("typing")
+.innerHTML+=text[i];
+
+
+i++;
+
+
+if(i>=text.length){
+
+clearInterval(timer);
+
+}
+
+
+},speed);
+
+
+}
+
+
+
+
+
+
+// HATI JATUH
+
+
+function buatHati(){
+
+
+setInterval(()=>{
+
+
+let h=document.createElement("div");
+
+
+h.className="hati";
+
+
+h.innerHTML="❤️";
+
+
+h.style.left=
+Math.random()*100+"%";
+
+
+h.style.animationDuration=
+(3+Math.random()*3)+"s";
+
+
+
+document.body.appendChild(h);
+
+
+
+setTimeout(()=>{
+
+h.remove();
+
+},6000);
+
+
+
+},200);
+
+
+
+}
+
+
+
+
+
+
+// MUSIC ON OFF
+
+
+function musik(){
+
+
+let music=
+document.getElementById("music");
+
+
+
+if(musicOn){
+
+
+music.pause();
+
+
+musicOn=false;
+
+
+}
+
+else{
+
+
+music.play();
+
+
+musicOn=true;
+
+
+}
+
+
+
+}
+
 
 
 
@@ -53,53 +219,7 @@ function surprise(){
 document.getElementById("pesan")
 .innerHTML=
 
-"💖 Terima kasih sudah menjadi seseorang yang spesial. Happy Birthday Berlian ❤️🎂";
-
-
-}
-
-
-
-function buatHati(){
-
-
-setInterval(()=>{
-
-
-let hati=document.createElement("div");
-
-
-hati.className="hati";
-
-hati.innerHTML="❤️";
-
-
-hati.style.left=
-Math.random()*100+"%";
-
-
-hati.style.fontSize=
-(Math.random()*30+10)+"px";
-
-
-hati.style.animationDuration=
-(Math.random()*3+3)+"s";
-
-
-document.body.appendChild(hati);
-
-
-
-setTimeout(()=>{
-
-hati.remove();
-
-},5000);
-
-
-
-},300);
-
+"💖 Berlian, semoga selalu bahagia. Terima kasih sudah lahir ke dunia ❤️🎂";
 
 
 }
